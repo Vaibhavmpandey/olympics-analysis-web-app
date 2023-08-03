ad02bf7a901d4738e83798821f93f1b7254b30e7
vaibhav <vaibhavmpandey10@gmail.com>
2023-08-03 06:32:59 +0530

    Commented age distribution

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import helper
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.figure_factory as ff
+#import plotly.figure_factory as ff
 
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
